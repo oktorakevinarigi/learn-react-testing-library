12c40100c73584fbf041a10e20ebf5ba090ab074
Oktora Kevin Arigi <oktora.kevin.arigi@gmail.com>
2023-08-03 15:36:57 +0700

    belajar describe untuk mengelompokkan beberapa test dan juga bisa membuat nested untuk kelompok tertentu

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -2,22 +2,34 @@ import {render, screen} from '@testing-library/react';
 
 import { Greet } from './greet';
 
-test("Greet renders correctly", () => {
-  render(<Greet />)
-  const testElement = screen.getByText(/hello/i)
-  expect(testElement).toBeInTheDocument()
+describe("Greet", () => {
+  test("renders correctly", () => {
+    render(<Greet />)
+    const testElement = screen.getByText(/hello/i)
+    expect(testElement).toBeInTheDocument()
+  })
+
+  describe("Nested", () => {
+    test("renders with a name", () => {
+      render(<Greet name="Vishwas" />)
+      const testElement = screen.getByText('Hello Vishwas')
+      expect(testElement).toBeInTheDocument()
+    })
+  })
 })
 
-test("Greet renders with a name", () => {
-  render(<Greet name="Vishwas" />)
-  const testElement = screen.getByText('Hello Vishwas')
-  expect(testElement).toBeInTheDocument()
-})
-
-/* Contoh jika hanya ingin menjalankan test berikut dan test lain dalam file ini aka di skip */
+/* Contoh jika hanya ingin menjalankan test berikut dan test lain dalam file ini akan di skip */
 // test.only("Greet renders correctly", () => {
 // })
 
 /* Contoh jika ingin skip test dan yang lain dijalankan didalam file ini */
 // test.skip("Greet renders with a name", () => {
+// })
+
+/* Contoh Group juga dapat jika menjalankan hanya test berikut dan test lain dalam file ini akan di skip */
+// describe.only("Greet", () => {
+// })
+
+/* Contoh Group juga dapat jika ingin skip test dan yang lain dijalankan didalam file ini */
+// describe.skip("Greet", () => {
 // })
