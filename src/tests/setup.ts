fd3e6faf758bc356401046c4a7707d8bfbb065d2
Oktora Kevin Arigi <oktora.kevin.arigi@gmail.com>
2023-08-04 21:42:28 +0700

    belajar mock http

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -1,9 +1,20 @@
 import { expect, afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import matchers from '@testing-library/jest-dom/matchers';
+import { server } from '../mocks/server';
 
 expect.extend(matchers);
 
 afterEach(() => {
   cleanup();
 });
+
+// Establish API mocking before all tests.
+beforeAll(() => server.listen())
+
+// Reset any request handlers that we may add during the tests,
+// so they don't affect other tests.
+afterEach(() => server.resetHandlers())
+
+// Clean up after the tests are finished.
+afterAll(() => server.close())
