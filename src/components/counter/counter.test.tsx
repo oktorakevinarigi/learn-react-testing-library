d91b4e4199dd0acaaa80d15544baa8f132550c0f
Oktora Kevin Arigi <oktora.kevin.arigi@gmail.com>
2023-08-04 17:21:29 +0700

    belajar interaction keyboard

--- src/components/counter/counter.test.tsx
+++ src/components/counter/counter.test.tsx
@@ -15,16 +15,45 @@ describe("Counter", () => {
   test("renders a count of 0", () => {
     render(<Counter />)
     const countElement = screen.getByRole('heading')
-    expect(countElement).toHaveTextContent('0')
+    expect(countElement).toHaveTextContent('0') // toHaveTextContent ada text di DOM
   })
 
   test("renders a count of 1 after clicking the increment button", async () => {
-    user.setup()
+    user.setup() // initial user event
     render(<Counter />)
     
     const incrementButton = screen.getByRole('button', {name: "Increment"})
-    await user.click(incrementButton)
+    await user.click(incrementButton) // user click button
     const countElement = screen.getByRole('heading')
     expect(countElement).toHaveTextContent('1')
+  })
+
+  test("renders a count of 10 after clicking the set button", async () => {
+    user.setup()
+    render(<Counter />)
+    
+    const amountInput = screen.getByRole('spinbutton') // spinbutton untuk input number
+    await user.type(amountInput, '10') // user.type untuk user menginputkan data melalui keyboard
+    expect(amountInput).toHaveValue(10) // toHaveValue untuk mengetahui value
+
+    const setButton = screen.getByRole('button', {name: "Set"})
+    await user.click(setButton)
+    const countElement = screen.getByRole('heading')
+    expect(countElement).toHaveTextContent('10')
+  })
+
+  test("elemnts are focused in the right order", async () => {
+    user.setup()
+    render(<Counter />)
+    
+    const amountInput = screen.getByRole('spinbutton')
+    const setButton = screen.getByRole('button', {name: "Set"})
+    const incrementButton = screen.getByRole('button', {name: "Increment"})
+    await user.tab() // user tab maka element focus
+    expect(incrementButton).toHaveFocus() // apakah element focus
+    await user.tab()
+    expect(amountInput).toHaveFocus()
+    await user.tab()
+    expect(setButton).toHaveFocus()
   })
 })
--- src/components/counter/counter.tsx
+++ src/components/counter/counter.tsx
@@ -2,10 +2,19 @@ import {useState} from 'react'
 
 export function Counter() {
   const [count, setCount] = useState(0)
+  const [amout, setAmount] = useState(0)
+
   return (
     <div>
       <h1>{count}</h1>
       <button onClick={() => setCount(count + 1)}>Increment</button>
+      <input 
+        type="number" 
+        name='amount' 
+        value={amout} 
+        onChange={(e) => setAmount(parseInt(e.target.value))} 
+      />
+      <button onClick={() => setCount(amout)}>Set</button>
     </div>
   )
 }
